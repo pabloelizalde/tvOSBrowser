25d5eaed48d46777faa8459ef37cc9eb709ec221
Pablo Elizalde <pelizcar@gmail.com>
2017-01-25 16:13:06 +0100

    Update url to Buddybuild dashboard

--- Browser/ViewController.m
+++ Browser/ViewController.m
@@ -92,7 +92,7 @@ typedef struct _Input
         [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[[NSUserDefaults standardUserDefaults] stringForKey:@"homepage"]]]];
     }
     else {
-        [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString: @"http://www.google.com"]]];
+        [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString: @"https://dashboard.buddybuild.com"]]];
     }
 }
 -(void)viewDidLoad {
